1a64e301a6f8ec05c818fc5767f2d744879fe86d
jmhogan <jmhogan@fnal.gov>
2024-10-24 10:01:09 -0500

    confusionPlots.py

--- plotting/confusionPlots.py
+++ plotting/confusionPlots.py
@@ -6,18 +6,14 @@ if readFile:
     inFile = TFile.Open("root://cmseos.fnal.gov//store/user/jmanagan/BtoTW_Jan2024_pNetAlt3/RDF_BprimeBtoTW_M-1400_NWALO_TuneCP5_13TeV-madgraph-pythia8_2018_0.root")
 
     decaymodes_den = TH2D("B_decays_den",";reconstructed decay mode;true decay mode",4,0,4,4,0,4)
-    decaymodes_alt = TH2D("B_decays_alt",";reconstructed decay mode;true decay mode",4,0,4,4,0,4)
     decaymodes_num = TH2D("B_decays",";reconstructed decay mode;true decay mode",4,0,4,4,0,4)
     decaymodes_den.Sumw2()
     decaymodes_num.Sumw2()
-    decaymodes_alt.Sumw2()
 
     leptonmodes_den = TH2D("lepton_source_den",";reconstructed lepton source;true lepton source",2,0,2,2,0,2)
     leptonmodes_num = TH2D("lepton_source",";reconstructed lepton source;true lepton source",2,0,2,2,0,2)
-    leptonmodes_alt = TH2D("lepton_alt",";reconstructed lepton source;true lepton source",2,0,2,2,0,2)
     leptonmodes_den.Sumw2()
     leptonmodes_num.Sumw2()
-    leptonmodes_alt.Sumw2()
 
     t = inFile.Get("Events_Nominal")
 
@@ -61,43 +57,24 @@ if readFile:
         else:
             decaymodes_num.Fill(1.5,i)
 
-        if t.Bdecay_obs_alt == 1:
-            decaymodes_alt.Fill(0.5,i)
-        elif t.Bdecay_obs_alt == 2:
-            decaymodes_alt.Fill(2.5,i)
-        elif t.Bdecay_obs_alt == 3:
-            decaymodes_alt.Fill(3.5,i)
-        else:
-            decaymodes_alt.Fill(1.5,i)
-
-    
         if t.Bdecay_obs == 1 or t.Bdecay_obs == 4:
             leptonmodes_num.Fill(0.5,j)
         else:
             leptonmodes_num.Fill(1.5,j)
 
-        if t.Bdecay_obs_alt == 1 or t.Bdecay_obs_alt == 4:
-            leptonmodes_alt.Fill(0.5,j)
-        else:
-            leptonmodes_alt.Fill(1.5,j)
-            
     decaymodes_num.Divide(decaymodes_num, decaymodes_den, 1, 1, "B")
-    decaymodes_alt.Divide(decaymodes_alt, decaymodes_den, 1, 1, "B")
     leptonmodes_num.Divide(leptonmodes_num, leptonmodes_den, 1, 1, "B")
-    leptonmodes_alt.Divide(leptonmodes_alt, leptonmodes_den, 1, 1, "B")
 
-    histFile = TFile.Open("confusionPlots_Bprime1400_WorT.root", "recreate")
+    histFile = TFile.Open("confusionPlots_Bprime1400.root", "recreate")
 
     decaymodes_num.Write()
-    decaymodes_alt.Write()
     leptonmodes_num.Write()
-    leptonmodes_alt.Write()
 
     histFile.Write()
     histFile.Close()
 
 ## Read histograms from file
-histFile = TFile.Open("confusionPlots_Bprime1400_WorT.root")
+histFile = TFile.Open("confusionPlots_Bprime1400.root")
 
 decaymodes_num = histFile.Get("B_decays")
 decaymodes_alt = histFile.Get("B_decays_alt")
@@ -108,21 +85,17 @@ canv1 = TCanvas("c1","c1",800,600)
 xlabels = ['t jet','untagged t','W jet','untagged W']
 for ibin in range(1,decaymodes_num.GetNbinsX()+1):
     decaymodes_num.GetXaxis().SetBinLabel(ibin,xlabels[ibin-1])
-    decaymodes_alt.GetXaxis().SetBinLabel(ibin,xlabels[ibin-1])
     
 ylabels = ['boosted t','unboosted t','boosted W','unboosted W']
 for ibin in range(1,decaymodes_num.GetNbinsY()+1):
     decaymodes_num.GetYaxis().SetBinLabel(ibin,ylabels[ibin-1])
-    decaymodes_alt.GetYaxis().SetBinLabel(ibin,ylabels[ibin-1])
     
 gStyle.SetOptStat(0)
 gStyle.SetPaintTextFormat("1.2f")
 canv1.SetLeftMargin(0.15);
 decaymodes_num.Draw("colz texte")
-canv1.SaveAs("decaymodes_1400_pNetOrig_WorT.png")
+canv1.SaveAs("decaymodes_1400_pNetOrig.png")
     
-decaymodes_alt.Draw("colz texte")
-canv1.SaveAs("decaymodes_1400_pNetAlt_WorT.png")
 
 
     
@@ -130,17 +103,13 @@ canv2 = TCanvas("c2","c2",800,600)
 xlabels = ['reco W', 'reco T']
 for ibin in range(1,leptonmodes_num.GetNbinsX()+1):
     leptonmodes_num.GetXaxis().SetBinLabel(ibin,xlabels[ibin-1])
-    leptonmodes_alt.GetXaxis().SetBinLabel(ibin,xlabels[ibin-1])
     
 ylabels = ['lep W', 'lep T']
 for ibin in range(1,leptonmodes_num.GetNbinsY()+1):
     leptonmodes_num.GetYaxis().SetBinLabel(ibin,ylabels[ibin-1])
-    leptonmodes_alt.GetYaxis().SetBinLabel(ibin,ylabels[ibin-1])
 
 gStyle.SetOptStat(0)
 gStyle.SetPaintTextFormat("1.2f")
 leptonmodes_num.Draw("colz texte")
 canv2.SaveAs("leptonmodes_1400_pNetOrig.png")
 
-leptonmodes_alt.Draw("colz texte")
-canv2.SaveAs("leptonmodes_1400_pNetAlt.png")
